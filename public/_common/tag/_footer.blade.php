0c0fc88fb7569687d2d2da444ba765677210eed5
石田朋也 <tomy@ishidatomoonarinoMacBook-Air.local>
2021-03-05 04:47:29 +0900

    ホーム画面　デザイン変更

--- public/_common/tag/_footer.blade.php
+++ public/_common/tag/_footer.blade.php
@@ -1,41 +1,83 @@
 <?php
-$nav_users = App\User::where([['existence',true],['belongs_to','like',"%tokyo%"],['role','!=',1],['role','!=',3]])->get();
-$nav_artists = App\User::where([['existence',true],['belongs_to','like',"%tokyo%"],['role','!=',1],['role','!=',3],['login_id','!=','white'],['login_id','!=','other']])->get();
+$nav_users = App\User::where([['existence',true],['belongs_to','like',"%kyoto%"],['role','!=',1],['role','!=',3]])->get();
+$nav_artists = App\User::where([['existence',true],['belongs_to','like',"%kyoto%"],['role','!=',1],['role','!=',3],['login_id','!=','white'],['login_id','!=','other']])->get();
 ?>
-<section class="sdcp">
-	<!-- <dl>
-		<dt>
-			<h4>犬猫の殺処分減少を目指すプロジェクト</h4>
-		</dt>
-		<dd>
-			<a href="/sdcp">
-				<img src="/_common/image/icons/b_sdcp.png" alt="犬猫の殺処分減少を目指すプロジェクト SDCP RAY'S">
-			</a>
-		</dd>
-	</dl> -->
-</section>
-<footer>
 
+<footer>
+<div id="area_footer">
+<div class="wrap_footer clearfix">
+<ul class="for_pc ftr_address">
+	<a href="/"><li class="ftr_logo"></li></a>
+	<li class="mgt20">
+    <p>
+      〒151-0051
+      <br>東京都渋谷区千駄ヶ谷3-26-7 第12FMGビルB1F
+    </p>
+	</li>
+</ul>
+<ul class="for_pc ftr_navi_1st">
+	<li><a href="/">&#9632;&nbsp;HOME</a></li>
+</ul>
+<ul class="for_pc ftr_navi">
+	<li>&#9632;&nbsp;SYSTEM</li>
+	<li><a href="/price">・Price</a></li>
+	<li class="mgt20"><a href="/artists">&#9632;&nbsp;ARTIST</a></li>
+	<?php foreach($nav_artists as $user): ?>
+		<li><a href="/show_artist/<?php echo $user->id ?>">・<?php echo strtoupper($user->login_id) ?></a></li>
+	<?php endforeach; ?>
+</ul>
+<ul class="for_pc ftr_navi">
+	<li class="mgt20"><a href="/aboutus">&#9632;&nbsp;ABOUT US</a></li>
+	<li><a href="/blogs">・Blog</a></li>
+</ul>
+<ul class="for_pc ftr_navi">
+	<li>&#9632;&nbsp;TATTOO GALLERY</li>
+	<li><a href="/gallery">・Tattoo Gallery</a></li>
+	<?php foreach($nav_users as $user): ?>
+		<li><a href="/gallery/#<?php echo $user->login_id ?>" class="sub_item">・<?php echo strtoupper($user->login_id) ?></a></li>
+	<?php endforeach; ?>
+	<li class="mgt20"><a href="/contact">&#9632;&nbsp;CONTACT</a></li>
+</ul>
+
+</div>
+<a href="/"><div class="for_sp ftr_logo"></div></a>
+
+<ul class="common_hdr_nav_02">
+    <a href="https://tattoo-rays.tw/"><li>台北STUDIO</li></a>
+		<a href="https://tattoo-rays.com"> <li>京都STUDIO</li> </a>
+</ul>
+
+<ul class="common_hdr_nav">
+	<a href="/zh"><li>中文</li></a>
+	<a href="/en"><li>English</li></a>
+	<a href="/#pickup_05"><li>Access</li></a>
+</ul>
+</div>
+<div class="for_sp mb-5" style="height: 50px;"></div>
+<div class="area_copy"><small><span class="for_pc">京都のタトゥー（tattoo）スタジオレイズ・刺青&emsp;</span>&copy;TATTOO STUDIO Ray's All Rights Reserved.</small></div>
 </footer>
 <div id="gMenu" class="for_sp uk-offcanvas">
     <div id="gMenuInner" class="uk-offcanvas-bar clearfix">
         <ul class="uk-nav uk-nav-offcanvas uk-nav-parent-icon" data-uk-nav>
         	<li><a href="/">&#9632;&nbsp;HOME</a></li>
-          <li><a class="main_item">&#9632;&nbsp;SYSTEM</a></li>
-          <li><a href="/price" class="sub_item">Price</a></li>
         	<li><a class="main_item">&#9632;&nbsp;GALLERY&emsp;ギャラリー</a></li>
         	<li><a href="/gallery" class="sub_item">Gallery</a></li>
 					<?php foreach($nav_users as $user): ?>
 						<li><a href="/gallery/#<?php echo $user->login_id ?>" class="sub_item"><?php echo strtoupper($user->login_id) ?></a></li>
 					<?php endforeach; ?>
+        	<li><a class="main_item">&#9632;&nbsp;SYSTEM&emsp;システム</a></li>
+        	<li><a href="/price" class="sub_item">Price&emsp;施術料金</a></li>
         	<li><a href="/artists" class="main_item">&#9632;&nbsp;ARTIST&emsp;アーティスト</a></li>
 					<?php foreach($nav_artists as $user): ?>
 						<li><a href="/show_artist/<?php echo $user->id ?>" class="sub_item"><?php echo strtoupper($user->login_id) ?></a></li>
 					<?php endforeach; ?>
-          <li><a href="/aboutus">&#9632;&nbsp;ABOUT US</a></li>
-          <li><a href="/blogs">Blog</a></li>
-          <li><a href="/contact">&#9632;&nbsp;CONTACT</a></li>
+
+        	<li><a href="/aboutus" class="main_item">&#9632;&nbsp;ABOUT US&emsp;コンセプト</a></li>
+        	<li><a href="/blogs">&#9632;&nbsp;BLOG&emsp;ブログ</a></li>
+        	<li><a href="/contact">&#9632;&nbsp;CONTACT&emsp;ご予約・お問い合わせ</a></li>
         	<li><a href="https://raysco.shop/" target="_blank" rel="nofollow noreferrer">&#9632;&nbsp;ONLINE STORE&emsp;オンラインストア</a></li>
+        	<li><a href="/en">&#9632;&nbsp;ENGLISH</a></li>
+        	<li><a href="/zh">&#9632;&nbsp;中文</a></li>
         </ul>
     </div>
 </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.page')
-<!-- Demo styles -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<link rel="stylesheet" href="/_common/css/dropdown.css">
+<link rel="stylesheet" href="/css/basic.css">
 <style media="screen">
   ul.common_hdr_nav_02, ul.common_hdr_nav{
     display: inline-table;
@@ -10,8 +16,36 @@
   .back_home_logo{
     display: none;
   }
+  .swiper-container {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+.swiper-slide img{
+  width: 98%;
+}
 </style>
 <?php
+  include("./_common/tag/top.html");
 	include("./_common/tag/_header.html");
 ?>
 @section('content')
@@ -51,47 +85,118 @@
     <p style="text-align: left;">す。</p>
   </div>
   <hr>
-
-  <h4 id="pickup_05">TATTOO STUDIO Ray’s TOKYO</h4>
-  <p class="address">〒151-0051 <br>東京都渋谷区千駄ヶ谷3-26-7 第12FMGビルB1F</p>
-  <a href="https://goo.gl/maps/9UrbSRFEazBY94LRA" class="center">
-    <div class="btn_box width_30 mb-3" style="font-size:.8rem; margin:0 auto;">
-      ＞ Google Map
+  <section>
+    <h2 class="impact">MAIN ARTIST <br>-Daniel Ray Brewster-</h2>
+    <ul class="area_top_artist clearfix">
+      <li>
+        <div class="top_bg_02">
+          <p class="c_txt">
+            『タトゥーは、一生に一度の大切なもの』です。
+            <br>お客様が納得いくまで何度でもデザインいたします。過去に自分の納得のいかない施術を受けた経験があり、皆様には同じ思いをしてもらいたくない。
+            <br>お客様1人1人のニーズを大切にし、悔いの無い一生物の作品を仕上げることを信念に、強い決意を持って施術しております。
+          </p>
+          <div class="btn_arw_01"><a href="/artists">ARTIST 紹介</a></div>
+        </div>
+      </li>
+      <li><div class="pic_artist"></div></li>
+    </ul>
+  </section>
+  <section>
+    <h2 class="impact">Studio Access</h2>
+    <div class="for_pc mgt80"></div>
+    <dl class="top_access_info">
+      <dd>
+        <p class="c_txt">タトゥーのご相談、施術は完全予約制となっておりますので、事前にご予約をお願い致します。</p>
+        <?php
+        include("./_common/tag/contact_btn.html");
+        ?>
+        <p class="c_txt">TATTOO STUDIO Ray’s TOKYO</p>
+        <p class="c_txt">
+          〒151-0051
+          <br>東京都渋谷区千駄ヶ谷3-26-7 第12FMGビルB1F
+        </p>
+        <?php
+        include("./_common/tag/map.html");
+        ?>
+        <p class="c_txt">
+          【電車でお越しの方】
+          <br>副都心線「北参道駅」徒歩3分
+          <br>総武中央線「千駄ヶ谷駅」徒歩8分
+        </p>
+      </dd>
+    </dl>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_01.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_02.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_03.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_04.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_05.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_06.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_07.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_08.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_09.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_10.jpg" alt=""></div>
+        <div class="swiper-slide"><img src="/_common/image/top/tokyo_11.jpg" alt=""></div>
+      </div>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination"></div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
     </div>
-  </a>
-  <p>【電車でお越しの方】</p>
-  <p class="mb-5">副都心線「北参道駅」徒歩3分 <br>
-  総武中央線「千駄ヶ谷駅」徒歩8分</p>
 
-  <a href="https://lin.ee/6yIXkH5" class="mt-5">
-    <p class="sns_p">
-        LINE&nbsp;&nbsp;@tattoorays.tokyo
-    </p>
-  </a>
-  <a href="https://twitter.com/rays_co">
-    <p class="sns_p">
-      Twitter&nbsp;&nbsp;@rays_co
-    </p>
-  </a>
-  <div class="insta">
-    <div id="insta-widget" class="iswg-base" data-username="tattoorays.tokyo" data-display-image-count="9" data-wrapper-width></div>
-    <script src="https://cdn.jsdelivr.net/gh/akinov/insta_window@v1.0/dist/main.js"></script>
-  </div>
-
-  <hr>
-  <a href="https://tattoo-rays.com/">
-    <div class="btn_box pc_wide mt-2">
-      KYOTO STUDIO
+    <div class="article_block">
+      <p class="c_txt">
+        白を基調とした店内は、明るく清潔感があり天井も高く開放的な雰囲気で徹底した衛生管理の元、皆様にタトゥー施術をリラックスして楽しんでいただけるようにしております。
+      </p>
     </div>
-  </a>
-  <a href="https://tattoo-rays.tw/">
-    <div class="btn_box pc_wide mt-5">
-      TAIPEI STUDIO
-    </div>
-  </a>
 
-  <hr>
-</section>
+    <div class="box_widget">
+
+      <div class="inner">
+        <h2>instagram</h2>
+        <div id="insta-widget" class="iswg-base" data-username="tattoostudiorays" data-display-image-count="9" data-wrapper-width></div>
+        <script src="https://cdn.jsdelivr.net/gh/akinov/insta_window@v1.0/dist/main.js"></script>
+
+      </div>
+
+    	<div class="inner">
+    		<h2>Blog & News</h2>
+    		<ul class="blogs mtb-5vh mlr-20px">
+    			<?php  foreach ($articles as $article): ?>
+            <a href="/blog/<?php echo $article->id ?>">
+    			   <li>
+              <?php if($article->thumbnail): ?>
+              <img src="<?php echo $article->thumbnail?>" alt="">
+              <?php else: ?>
+                <div style="width:100px;height:100px;"></div>
+              <?php endif; ?>
+              <div class="wrapper">
+                <div class="blogs_created_at">
+                  <?php echo (new DateTime($article->created_at))->format("Y.m.d") ?>
+                </div>
+                <div class="blogs_title">Title: <?php echo $article->title?></div>
+                <div class="blogs_body"><?php echo mb_strcut(strip_tags(str_replace("\r\n", '', $article->body)),0,100) ?></div>
+              </div>
+    			  </li>
+          </a>
+    			<hr>
+    		<?php endforeach; ?>
+    		</ul>
+    	</div>
+
+    	<div class="inner">
+    		<h2>Twitter</h2>
+    		<div class="box_twitter">
+    			<a class="twitter-timeline" href="https://twitter.com/rays_co?ref_src=twsrc%5Etfw">Tweets by rays_co</a>
+    			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    		<div>
+    	</div>
+    </div>
+
+  </section>
+
 <section class="footer">
 
     <ul>
@@ -127,26 +232,22 @@
         </a>
       </li>
     </ul>
-
-<!--
-    <img src="/images/tmp/sdcp.png" class="sdcp center mt-5 mb-5">
-    <img src="/images/tmp/footer_emb.png" alt="" class="footer_emb">
-    <p>東京都渋谷区千駄ヶ谷3-26-7 第12FMGビルB1F <br> TEL : </p>
-    <img src="/images/tmp/card.png" class="width_70 center mb-5">
-    <a href="https://tattoo-rays.com">    <img src="images/tmp/kyoto_head_btn.png" class="mb-3"></a>
-    <a href="https://tattoo-rays.tw">    <img src="/images/tmp/taipei_studio_btn.png" class="mb-5"></a>
-
-    <div class="footer_nav">
-      <img src="/images/tmp/tyuubun.png" alt="">
-      <img src="/images/tmp/english.png" alt="">
-      <img src="/images/tmp/access.png" alt="">
-    </div>
-    <hr>
-    <div class="copyright">©TATTOO STUDIO Ray's All Rights Reserved.</div> -->
 </section>
+
 <?php
 include("./_common/tag/_footer.blade.php");
 include("./_common/tag/script.html");
 ?>
-
+<script>
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+</script>
 @endsection
