607db45df96305b9bc2b8266fcd21efe689d5a68
ice-crypto <testtest2tom@gmail.com>
2021-04-15 17:25:28 +0900

    修正#11値段改正

--- resources/views/pages/price.blade.php
+++ resources/views/pages/price.blade.php
@@ -21,11 +21,12 @@ $page_title = "Price ";
 		<div class="article_block area_price">
 
 			<p class="c_txt price_img">
-				<img src="/_common/image/price/price_ja.png" alt="合計金額">
+				<img src="/_common/image/price/price_ja.jpg" alt="合計金額">
 			</p>
 			<p class="c_txt">
-				&#9632;料金はサイズ制ですが、デザイン内容によって変わります。
-				<br>&#9632;1回の施術につき、消耗品代金として4,000円が必要です。
+				&#9632;料金はサイズ制ですが、デザイン内容によって変わります。<br>
+				&#9632;表示料金は各サイズの税込最低料金です。
+				<br>&#9632;1回の施術につき、消耗品代金として4,400円（税込）が必要です。
 			</p>
 
 			<h3><span>料金の目安</span></h3>
@@ -39,22 +40,22 @@ $page_title = "Price ";
 				</tr>
 				<tr>
 					<th>500円玉<br class="for_sp">サイズ</th>
-					<td>&yen;15,000〜</td>
+					<td>&yen;16,500〜</td>
 					<td>&yen;10,000〜</td>
 				</tr>
 				<tr>
 					<th>タバコ<br class="for_sp">サイズ（5×9cm）</th>
-					<td>&yen;30,000〜</td>
+					<td>&yen;33,000〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
 					<th>スマホ<br class="for_sp">サイズ（7×13cm）</th>
-					<td>&yen;45,000〜</td>
+					<td>&yen;49,500〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
 					<th>ハガキ<br class="for_sp">サイズ（10×15cm）</th>
-					<td>&yen;55,000〜</td>
+					<td>&yen;60,500〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
@@ -75,22 +76,22 @@ $page_title = "Price ";
 				</tr>
 				<tr>
 					<th>500円玉<br class="for_sp">サイズ</th>
-					<td>&yen;20,000〜</td>
+					<td>&yen;22,000〜</td>
 					<td>&yen;10,000〜</td>
 				</tr>
 				<tr>
 					<th>タバコ<br class="for_sp">サイズ（5×9cm）</th>
-					<td>&yen;40,000〜</td>
+					<td>&yen;44,000〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
 					<th>スマホ<br class="for_sp">サイズ（7×13cm）</th>
-					<td>&yen;55,000〜</td>
+					<td>&yen;60,500〜</td>
 					<td>&yen;30,000〜</td>
 				</tr>
 				<tr>
 					<th>ハガキ<br class="for_sp">サイズ（10×15cm）</th>
-					<td>&yen;65,000〜</td>
+					<td>&yen;71,500〜</td>
 					<td>&yen;30,000〜</td>
 				</tr>
 				<tr>
@@ -113,25 +114,25 @@ $page_title = "Price ";
 				<tr>
 					<th>500円玉<br class="for_sp">サイズ</th>
 					<td class="emp"></td>
-					<td>&yen;60,000〜</td>
+					<td>&yen;66,000〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
 					<th>タバコ<br class="for_sp">サイズ（5×9cm）</th>
-					<td>&yen;60,000〜</td>
-					<td>&yen;60,000〜</td>
+					<td>&yen;66,000〜</td>
+					<td>&yen;66,000〜</td>
 					<td>&yen;20,000〜</td>
 				</tr>
 				<tr>
 					<th>スマホ<br class="for_sp">サイズ（7×13cm）</th>
-					<td>&yen;75,000〜</td>
-					<td>&yen;80,000〜</td>
+					<td>&yen;82,000〜</td>
+					<td>&yen;88,000〜</td>
 					<td>&yen;30,000〜</td>
 				</tr>
 				<tr>
 					<th>ハガキ<br class="for_sp">サイズ（10×15cm）</th>
-					<td>&yen;95,000〜</td>
-					<td>&yen;110,000〜</td>
+					<td>&yen;104,500〜</td>
+					<td>&yen;121,000〜</td>
 					<td>&yen;30,000〜</td>
 				</tr>
 				<tr>
@@ -145,6 +146,7 @@ $page_title = "Price ";
 				&#9632; Deposit（預り金）：施術費用の一部を予約金としてお預り致します。
 				<br>&#9632; ラフデザインはDepositの入金後に制作開始致します。
 				<br>&#9632; Deposit入金後、施術をキャンセルされましても<br class="for_pc">ご返金致し兼ねますのでご了承ください。
+				<br>&#9632; ONLINEカウンセリングのDepositは30,000〜となります。
 			</p>
 
 			<div class="mgb80"></div>
